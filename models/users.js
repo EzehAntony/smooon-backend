9127610489f7a4cd4a9e291b28017d5273a465a2
Anthony <58951485+EzehAntony@users.noreply.github.com>
2022-10-01 05:37:44 +0100

    backend bug

--- models/users.js
+++ models/users.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const User = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
@@ -20,4 +20,4 @@ const User = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", User);
+module.exports = mongoose.model("user", userSchema);
